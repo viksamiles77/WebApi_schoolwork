c50719f3141089c37c7373ff68a3e5788bbaa0a4
viksamiles77 <mileskiviktor77@gmail.com>
2024-08-05 20:51:06 +0200

    Class01

---
+++ Class01/Class01.OurFirstApi/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Class01.OurFirstApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
