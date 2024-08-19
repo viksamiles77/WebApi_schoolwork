db71769fe49888461748a8ecb29a71967d8ecdb6
viksamiles77 <mileskiviktor77@gmail.com>
2024-08-19 16:00:56 +0200

    Create MovieUpdateDto.cs

---
+++ Class05-Workshop/Qinshift.Movies/Qinshift.Movies.DTOs/MovieUpdateDto.cs
@@ -0,0 +1,24 @@
+﻿using Qinshift.Movies.DomainModels.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Qinshift.Movies.DTOs
+{
+    public class MovieUpdateDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [MaxLength(250)]
+        public string Description { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        public GenreEnum Genre { get; set; }
+    }
+}
