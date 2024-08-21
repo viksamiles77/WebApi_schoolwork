1710d0541fb265362a547b2f5d207d1d0e55f13d
viksamiles77 <mileskiviktor77@gmail.com>
2024-08-21 18:18:13 +0200

    Update Contact.cs

--- Class06/Qinshift.Class06/DataAccess/Domain/Contact.cs
+++ Class06/Qinshift.Class06/DataAccess/Domain/Contact.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Qinshit.DataAccess.Domain
+namespace Qinshift.DataAccess.Domain
 {
     [Table("Contacts")]
     public class Contact
