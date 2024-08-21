db9b69ac8055218a80166ede79b48eeac25ae423
viksamiles77 <mileskiviktor77@gmail.com>
2024-08-21 18:19:30 +0200

    Update User.cs

--- Class06/Qinshift.Class06/DataAccess/Domain/User.cs
+++ Class06/Qinshift.Class06/DataAccess/Domain/User.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Qinshift.DataAccess.Domain
@@ -7,19 +8,22 @@ namespace Qinshift.DataAccess.Domain
     {
         public User()
         {
-            Notes = new HashSet<Note>();
+            Notes = new List<Note>();
+            Meals = new List<Meal>();
+            Contacts = new List<Contact>();
         }
-        [Key]
+
         public int Id { get; set; }
         public string? Username { get; set; }
         public string? Password { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public int Age { get; set; }
-
         public virtual ICollection<Note> Notes { get; set; }
+
         [InverseProperty("User")]
         public virtual ICollection<Contact> Contacts { get; set; }
+
         public virtual ICollection<Meal> Meals { get; set; }
     }
 }
