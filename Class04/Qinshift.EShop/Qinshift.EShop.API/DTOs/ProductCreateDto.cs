de6e86800bf5a0e27bcfc2acc97b86abbea6901b
viksamiles77 <mileskiviktor77@gmail.com>
2024-08-21 18:08:59 +0200

    Update ProductCreateDto.cs

--- Class04/Qinshift.EShop/Qinshift.EShop.API/DTOs/ProductCreateDto.cs
+++ Class04/Qinshift.EShop/Qinshift.EShop.API/DTOs/ProductCreateDto.cs
@@ -7,8 +7,9 @@ namespace Qinshift.EShop.API.DTOs
         [Required(ErrorMessage = "Name cannot be empty!")]
         [MinLength(3, ErrorMessage = "Name must contain at least 3 characters!")]
         public string Name { get; set; }
+        [MaxLength(250)]
         public string Description { get; set; }
-        public int Price { get; set; }
+        public decimal Price { get; set; }
         public int StockQuantity { get; set; }
         public string ImageUrl { get; set; }
         public int CategoryId { get; set; }
