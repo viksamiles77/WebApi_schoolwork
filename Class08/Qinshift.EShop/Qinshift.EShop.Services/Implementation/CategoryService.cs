951af484b154e4e10393e2744e51e7045ac18aeb
viksamiles77 <mileskiviktor77@gmail.com>
2024-08-24 11:30:06 +0200

    Class08 / EShop with ADO

---
+++ Class08/Qinshift.EShop/Qinshift.EShop.Services/Implementation/CategoryService.cs
@@ -0,0 +1,31 @@
+﻿using Qinshift.EShop.DataAccess.Interface;
+using Qinshift.EShop.DomainModels;
+using Qinshift.EShop.DTOs.Category;
+using Qinshift.EShop.Services.Interface;
+
+namespace Qinshift.EShop.Services.Implementation
+{
+    public class CategoryService : ICategoryService
+    {
+        //private readonly ICategoryRepository _categoryRepo;
+
+        //public CategoryService(ICategoryRepository categoryRepo)
+        //{
+        //    _categoryRepo = categoryRepo;
+        //}
+
+        private readonly IRepository<Category> _categoryRepo;
+        public IEnumerable<CategoryDto> GetAllCategories()
+        {
+            return _categoryRepo.GetAll().Select(x => new CategoryDto
+            { Name = x.Name });
+        }
+
+        public CategoryDto GetCategoryById(int id)
+        {
+            var category = _categoryRepo.GetById(id);
+            var categoryDto = new CategoryDto { Name = category.Name };
+            return categoryDto;
+        }
+    }
+}
