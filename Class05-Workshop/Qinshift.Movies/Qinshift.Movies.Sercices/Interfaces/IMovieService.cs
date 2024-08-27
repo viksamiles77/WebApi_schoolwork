871a935b6a866a6d1723b0cc16fa43a3b3d72caa
viksamiles77 <mileskiviktor77@gmail.com>
2024-08-27 02:55:52 +0200

    Update IMoveService.cs
    fixed UpdateMovie and FilterMoviesByYear to return MovieDto

--- Class05-Workshop/Qinshift.Movies/Qinshift.Movies.Sercices/Interfaces/IMovieService.cs
+++ Class05-Workshop/Qinshift.Movies/Qinshift.Movies.Sercices/Interfaces/IMovieService.cs
@@ -1,5 +1,4 @@
-﻿using Qinshift.Movies.DomainModels;
-using Qinshift.Movies.DTOs;
+﻿using Qinshift.Movies.DTOs;
 
 namespace Qinshift.Movies.Services.Implementation
 {
@@ -8,9 +7,9 @@ namespace Qinshift.Movies.Services.Implementation
         List<MovieDto> GetAllMovies();
         MovieDto GetMovieById(int id);
         List<MovieDto> FilterMoviesByGenre(string genre);
-        List<Movie> FilterMoviesByYear(int year);
+        List<MovieDto> FilterMoviesByYear(int year);
         int AddNewMovie(MovieCreateDto movie);
-        Movie UpdateMovie(Movie movie);
+        MovieDto UpdateMovie(int id, MovieUpdateDto updatedMovie);
         int DeleteMovieById(int id);
         void DeleteMovieByIdFromBody(int id);
     }
